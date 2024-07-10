18078721b245de213625a31ea52753e155c890d8
Jessica Bruna de Oliveira <jazzkoliver@gmail.com>
2024-07-10 20:25:59 -0300

    Segundo plano de teste

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # automacao-ui-cypress-1
+
+Primeira automação de UI no Cypress do curso EBAC por Jessica Oliveira
---
+++ cypress/e2e/loja-ebac/cadastro.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress"/>
+import { faker } from '@faker-js/faker';
+
+describe('Cadastro', () => {
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+    it('Cadastro com sucesso', () => {
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type(faker.internet.password())
+        cy.get(':nth-child(4) > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+    });
+
+    it('Usuário já cadastrado', () => {
+        cy.get('#reg_email').type('jazzqa@gmail.com')
+        cy.get('#reg_password').type(faker.internet.password())
+        cy.get(':nth-child(4) > .button').click()
+
+        cy.get('.woocommerce-error').should('contain', 'Erro: Uma conta já está registrada com seu endereço de e-mail.')
+        
+    });
+});
