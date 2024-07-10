0cdedb25b4c595f62490f3069c0685a70d831fd6
Jessica Bruna de Oliveira <jazzkoliver@gmail.com>
2024-07-10 19:52:54 -0300

    Estrutura do projeto + primeiro teste automatizado

---
+++ cypress/e2e/loja-ebac/login.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress"/>
+
+describe('Login', () => {
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('jazzqa@gmail.com')
+        cy.get('#password').type('abc123')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(3)').should('contain', 'A partir do painel de controle de sua conta, você pode ver suas compras recentes, gerenciar seus endereços de entrega e faturamento, e editar sua senha e detalhes da conta.')
+    });
+});
