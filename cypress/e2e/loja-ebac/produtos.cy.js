c0f9f2ca19a68cc91b058089808c7030c73aa218
Jessica Bruna de Oliveira <jazzkoliver@gmail.com>
2024-07-10 20:36:06 -0300

    Terceiro plano de teste

--- cypress/e2e/loja-ebac/produtos.cy.js
+++ cypress/e2e/loja-ebac/produtos.cy.js
@@ -1,12 +1,15 @@
 /// <reference types="cypress"/>
 
 describe('Produtos', () => {
-    it('Deve fazer login com sucesso', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-        cy.get('#username').type('jazzqa@gmail.com')
-        cy.get('#password').type('abc123')
-        cy.get('.woocommerce-form > .button').click()
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
 
-        cy.get('.woocommerce-MyAccount-content > :nth-child(3)').should('contain', 'A partir do painel de controle de sua conta, você pode ver suas compras recentes, gerenciar seus endereços de entrega e faturamento, e editar sua senha e detalhes da conta.')
+    afterEach(() => {
+        cy.screenshot()
+    });
+    it('Selecionar um produto da lista', () => {
+        cy.get('.product-block > .block-inner > .image > .product-image > .image-hover').first().click()
+        cy.get('.product_title').should('exist')
     });
 });
