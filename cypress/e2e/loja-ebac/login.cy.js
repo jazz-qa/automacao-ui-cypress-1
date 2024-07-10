bc77adb35414753dee349f9811fd2ffa735e8d30
Jessica Bruna de Oliveira <jazzkoliver@gmail.com>
2024-07-10 20:05:38 -0300

    Adicionado mais 2 cenários de erro e hooks

--- cypress/e2e/loja-ebac/login.cy.js
+++ cypress/e2e/loja-ebac/login.cy.js
@@ -1,12 +1,34 @@
 /// <reference types="cypress"/>
 
 describe('Login', () => {
-    it('Deve fazer login com sucesso', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+    it('Login com sucesso', () => {
         cy.get('#username').type('jazzqa@gmail.com')
         cy.get('#password').type('abc123')
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.woocommerce-MyAccount-content > :nth-child(3)').should('contain', 'A partir do painel de controle de sua conta, você pode ver suas compras recentes, gerenciar seus endereços de entrega e faturamento, e editar sua senha e detalhes da conta.')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(3)').should('contain', 'A partir do painel de controle de sua conta')
+    });
+
+    it('Usuário com senha inválida', () => {
+        cy.get('#username').type('jazzqa@gmail.com')
+        cy.get('#password').type('abc122')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail')
+    });
+    
+    it('Usuário inválido', () => {
+        cy.get('#username').type('jazzk@gmail.com')
+        cy.get('#password').type('abc123')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido.')
     });
 });
